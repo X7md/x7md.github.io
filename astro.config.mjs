455d023da31a3ef79cabc329343a7e8f922090c2
x7md <me@x7md.net>
2022-04-26 06:16:24 +0300

    fix SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,23 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
-
 // @type-check enabled!
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
+import WindiCSS from 'vite-plugin-windicss'; // @ts-check
 
-import WindiCSS from 'vite-plugin-windicss'
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ (
-  {
-    // Set "renderers" to "[]" to disable all default, builtin component support.
-    // renderers: [],
-    vite: {
-      plugins: [WindiCSS()]
-    }
-  }
-);
+// https://astro.build/config
+export default defineConfig(
+/** @type {import('astro').AstroUserConfig} */
+{
+  // Set "renderers" to "[]" to disable all default, builtin component support.
+  // renderers: [],
+  vite: {
+    plugins: [WindiCSS()]
+  },
+  site: 'https://git.x7md.net/',
+  integrations: [sitemap()]
+});
