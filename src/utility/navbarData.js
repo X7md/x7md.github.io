2e92873a403744259761b964070e3280202cebcc
x7md <x7mdNet@gmail.com>
2022-07-29 05:11:58 +0300

    code refactor

---
+++ src/utility/navbarData.js
@@ -0,0 +1,6 @@
+export default [
+    {"title": "عني", "link": "/about-me"},
+	{"title": "التدوينات", "link": "/posts"}, 
+	{"title": "المواضيع", "link": "/topics"},   
+	{"title": "تواصل معي", "link": "/connect"}
+]
