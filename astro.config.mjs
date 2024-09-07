3189a19075e4408f0aa795f9fc9f5d4150c7f9ae
x7md <me@x7md.net>
2024-09-07 17:37:38 +0300

    add @rollup/rollup-linux-x64-gnu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+// import mdx from '@astrojs/mdx';
+// import sitemap from '@astrojs/sitemap';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeWrapAll from 'rehype-wrap-all';
 import rehypeRewrite from 'rehype-rewrite';
